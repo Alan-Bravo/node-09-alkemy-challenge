90c3e6f59fd226fa5ade65d27332921e8b522e5a
Alan <bravoalangonzalo@gmail.com>
2022-02-02 21:26:08 -0300

    first commit

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,29 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/users');
+
+class UserRepository {
+  constructor() {}
+
+  async findAll(filter, options) {
+    return await User.paginate(filter, options);
+  }
+
+  async findById(id) {
+    return await User.findById(id);
+  }
+
+  async save(user) {
+    user.password = await bcrypt.hash(user.password, 10);
+    return await User.create(user);
+  }
+
+  async update(id, user) {
+    return await User.findByIdAndUpdate(id, user, { new: true });
+  }
+
+  async remove(id) {
+    return await User.findByIdAndRemove(id);
+  }
+}
+
+module.exports = UserRepository;
